9640648c008e0ffc42db2a7b762a3a310509b1d2
YouChaeHyeon <ismy123@naver.com>
2019-11-13 19:52:11 +0900

    script
    scipt

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -7,7 +7,7 @@ public class Move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("울랄라");
+        Debug.Log("울랄라요");
     }
 
     // Update is called once per frame

a36e7e3b9c9d4538e3b39499232b3bea0c5514ce
YouChaeHyeon <ismy123@naver.com>
2019-11-18 22:10:20 +0900

    Road
    RoadGen에서 UPDATE 부분에 있는 길이동이 안도애

--- Assets/Scripts/RoadGen.cs
+++ Assets/Scripts/RoadGen.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class RoadGen : MonoBehaviour // 끝없는 길 만들기
 {
-    public Transform startPos;
+    public Transform startPos; // 길의 위치를 알아서 자연스럽게 이어지게 함
     int nextStep = 0;
 
-    float speed = 4.0f;
+    float speed = 4.0f; // 길이 움직이는 속도 조절
 
     Vector3 moveDir = Vector3.zero;
 
@@ -17,15 +17,16 @@ public class RoadGen : MonoBehaviour // 끝없는 길 만들기
     void Update()
     { 
         nextStep+=10;
-        GameObject moveroad = Instantiate(PrefabsRoad[Random.Range(0,PrefabsRoad.Length)], new Vector3(startPos.position.x,startPos.position.y,transform.position.z+nextStep), Quaternion.identity) as GameObject; // 길 생성
+        GameObject moveroad = Instantiate(PrefabsRoad[Random.Range(0,PrefabsRoad.Length)], new Vector3(startPos.position.x,startPos.position.y,transform.position.z+nextStep), Quaternion.identity) as GameObject; // 길 무한 생성
         
          if (Singleton.Instance.isWalking == true) // 사용자가 움직이면 길도 움직이게 하기
         {
-                // transform.position = new Vector3(0,0,-100.0f);//프레임마다 등속으로 다가오기
+            // moveroad.transform.position = new Vector3(0,0,-20.0f);//프레임마다 등속으로 다가오기
 
-                moveDir = new Vector3(0, 0, 1);
-                moveDir *= speed;
-                moveDir = transform.TransformDirection(moveDir);
+            moveDir = new Vector3(0, 0, -20);
+            moveDir *= speed;
+            moveDir = transform.TransformDirection(moveDir);
+
         }
 
         else if (Singleton.Instance.isWalking == false)
